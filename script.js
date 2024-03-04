77d4e3bdf3b36f777b63c3031000013bbe4cefd1
Jack Sarles <jack.sarles@mcmcg.com>
2024-03-04 12:35:31 -0800

    Intialize

---
+++ script.js
@@ -0,0 +1,6 @@
+//You’re going to store the gameboard as an array inside of a Gameboard object, so start there! Your players are also going to be stored in objects, and you’re probably going to want an object to control the flow of the game itself.
+//Your main goal here is to have as little global code as possible. Try tucking as much as you can inside factories. If you only need a single instance of something (e.g. the gameboard, the displayController etc.) then wrap the factory inside an IIFE (module pattern) so it cannot be reused to create additional instances.
+//n this project, think carefully about where each bit of logic should reside. Each little piece of functionality should be able to fit in the game, player or gameboard objects. 
+//Make sure you include logic that checks for when the game is over! You should be checking for all winning 3-in-a-rows and ties. 
+//create an object that will handle the display/DOM logic. Write a function that will render the contents of the gameboard array to the webpage
+//Write the functions that allow players to add marks to a specific spot on the board by interacting with the appropriate DOM elements (e.g. letting players click on a board square to place their marker). Don’t forget the logic that keeps players from playing in spots that are already taken!
